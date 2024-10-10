03aad112c63351aed72f6ad164e5c989f0e88ccc
stoffee <chris@roadkill.org>
2024-10-10 16:12:40 -0700

    alrighty

--- tf/main.tf
+++ tf/main.tf
@@ -29,38 +29,22 @@ resource "tls_private_key" "demo" {
 resource "aws_key_pair" "generated_key" {
   key_name   = "${var.prefix}-key"
   public_key = tls_private_key.demo.public_key_openssh
-
-  tags = {
-    Name = "${var.prefix}-key-pair"
-  }
 }
 
 resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "${var.prefix}-vpc"
-  }
 }
 
 resource "aws_subnet" "demo" {
   vpc_id            = aws_vpc.demo.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.region}a"
-
-  tags = {
-    Name = "${var.prefix}-subnet"
-  }
 }
 
 resource "aws_security_group" "demo" {
   name        = "${var.prefix}-sg"
   description = "Allow 22 and 80 for demo inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.demo.id
-
-  tags = {
-    Name = "${var.prefix}-security-group"
-  }
 }
 
 resource "aws_security_group_rule" "demo_app" {
@@ -81,13 +65,15 @@ resource "aws_security_group_rule" "demo_ssh" {
   security_group_id = aws_security_group.demo.id
 }
 
-resource "aws_security_group_rule" "demo_egress" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+# New rule for EC2 Instance Connect
+resource "aws_security_group_rule" "instance_connect" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["3.16.146.0/29", "3.22.11.0/29", "18.206.107.24/29", "3.80.101.78/32", "3.91.186.242/32", "3.132.215.46/32"]
   security_group_id = aws_security_group.demo.id
+  description       = "Allow EC2 Instance Connect"
 }
 
 data "aws_ami" "ubuntu" {
@@ -103,15 +89,46 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+# New IAM role for EC2 Instance Connect
+resource "aws_iam_role" "instance_connect_role" {
+  name = "${var.prefix}-instance-connect-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach policy to allow EC2 Instance Connect
+resource "aws_iam_role_policy_attachment" "instance_connect_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+  role       = aws_iam_role.instance_connect_role.name
+}
+
+# Create instance profile
+resource "aws_iam_instance_profile" "instance_connect_profile" {
+  name = "${var.prefix}-instance-connect-profile"
+  role = aws_iam_role.instance_connect_role.name
+}
+
 resource "aws_instance" "demo" {
   ami                         = data.aws_ami.ubuntu.id
   availability_zone           = "${var.region}a"
   instance_type               = "t2.small"
-  associate_public_ip_address = true
+  associate_public_ip_address = "true"
   key_name                    = aws_key_pair.generated_key.key_name
   vpc_security_group_ids      = [aws_security_group.demo.id]
   subnet_id                   = aws_subnet.demo.id
   user_data                   = data.template_file.cloud-init.rendered
+  iam_instance_profile        = aws_iam_instance_profile.instance_connect_profile.name
 
   tags = {
     Name = "${var.prefix}-instance"
@@ -122,18 +139,6 @@ data "template_file" "cloud-init" {
   template = file("cloud-init.tpl")
 }
 
-data "external" "cloud_init_logs" {
-  program = ["bash", "-c", <<EOT
-    sleep 180 &&
-    cloud_init_output=$(ssh -o StrictHostKeyChecking=no -i <(echo '${tls_private_key.demo.private_key_pem}') ubuntu@${aws_instance.demo.public_ip} 'sudo cat /var/log/cloud-init-output.log') &&
-    cloud_init_log=$(ssh -o StrictHostKeyChecking=no -i <(echo '${tls_private_key.demo.private_key_pem}') ubuntu@${aws_instance.demo.public_ip} 'sudo cat /var/log/cloud-init.log') &&
-    echo "{\"cloud_init_output\": \"$cloud_init_output\", \"cloud_init_log\": \"$cloud_init_log\"}" | jq -R -s -c '{"cloud_init_output": ., "cloud_init_log": .}'
-  EOT
-  ]
-
-  depends_on = [aws_instance.demo]
-}
-
 output "aws_instance_login_information" {
   value = <<INSTANCEIP
   http://${aws_instance.demo.public_ip}
@@ -142,12 +147,4 @@ INSTANCEIP
 
 output "aws_key_pair_info" {
   value = nonsensitive(tls_private_key.demo.private_key_openssh)
-}
-
-output "cloud_init_output_log" {
-  value = data.external.cloud_init_logs.result.cloud_init_output
-}
-
-output "cloud_init_log" {
-  value = data.external.cloud_init_logs.result.cloud_init_log
 }

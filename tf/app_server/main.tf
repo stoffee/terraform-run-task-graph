dcb527fcd470189dded37a5187d3502c0509cc92
stoffee <chris@roadkill.org>
2024-10-10 18:37:36 -0700

    sleep a little

--- tf/app_server/main.tf
+++ tf/app_server/main.tf
@@ -167,7 +167,13 @@ data "template_file" "cloud-init" {
   template = file("cloud-init.tpl")
 }
 
+resource "time_sleep" "wait_3_minutes" {
+  depends_on = [aws_instance.app]
+  create_duration = "180s"
+}
+
 resource "tfe_organization_run_task" "app_task" {
+  depends_on = [time_sleep.wait_3_minutes]
   organization = var.tfe_organization
   url          = "http://${aws_instance.app.public_ip}"
   name         = "${var.prefix}-run-task"
